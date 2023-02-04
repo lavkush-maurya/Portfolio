b59e3f6d3b7f9f39470bdd84b20058287786c83a
Lavkush Maurya <113632168+lavkush-maurya@users.noreply.github.com>
2023-02-04 22:36:30 +0530

    Add files via upload

---
+++ js/classie.js
@@ -0,0 +1,126 @@
+/*
+██╗  ██╗███████╗██╗     ██╗      ██████╗     ██╗    ██╗ ██████╗ ██████╗ ██╗     ██████╗
+██║  ██║██╔════╝██║     ██║     ██╔═══██╗    ██║    ██║██╔═══██╗██╔══██╗██║     ██╔══██╗
+███████║█████╗  ██║     ██║     ██║   ██║    ██║ █╗ ██║██║   ██║██████╔╝██║     ██║  ██║
+██╔══██║██╔══╝  ██║     ██║     ██║   ██║    ██║███╗██║██║   ██║██╔══██╗██║     ██║  ██║
+██║  ██║███████╗███████╗███████╗╚██████╔╝    ╚███╔███╔╝╚██████╔╝██║  ██║███████╗██████╔╝
+╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝      ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝
+
+Greetings !
+
+if (employer) {
+  yes, I'm always looking for great opportunities;
+}
+else if (want a website) {
+  ping me ! lets discuss how will I help you;
+}
+else if (geek) {
+  hi, I love to be friends with like minded people.
+  Message me, maybe we could help each other.
+}
+else {
+  goto: www.linkedin.com/in/lavkushmaurya04
+}
+________________________________________________________________________________________
+
+When I wrote this code, only God and I understood what I was doing
+Now, God only knows
+
+If you don't like anything on this website, bite me !
+________________________________________________________________________________________
+
+No copyright issues.
+Feel free to copy anything and everything from this website.
+If you need any help, ping me !
+________________________________________________________________________________________
+
+Cheers,
+Lavkush Maurya
+mailto: lavkushmaurya147@gmail.com
+________________________________________________________________________________________
+
+I sincerely admit ! Not everything thing you saw on the website was developed by me from
+scratch, most of it is my original creation otherwise some of the elements are inspired
+or reproduced from the open source public code.
+
+SSSssshhhhh!! The code starts below */
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
